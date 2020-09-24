c9f4998cc12b87a09899e0f4dfc2c494ec29bb54
unknown <wearglitz@gmail.com>
2020-09-24 18:59:31 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-module.exports = require("./src/masterdb.js");
+const sqlite3 = require('sqlite3').verbose();
+const sql = new sqlite3.Database('./database.db')
+
+module.exports = {
+    allQuery: async function(query, args) {
+        if(!query) return console.error('No has ingresado una consulta')
+        try {
+            var result =
+                new Promise((resolve, reject) => {
+                    sql.all(query, args, function(err, row) {
+                        if (err) reject(err);
+                        resolve(row)
+                    })
+                })
+
+
+        } catch (e) {
+            console.error(e)
+        }
+        return result;
+    },
+    getQuery: async function(query, args) {
+        if(!query) return console.error('No has ingresado una consulta')
+        try {
+            var result =
+                new Promise((resolve, reject) => {
+                    sql.get(query, args, function(err, row) {
+                        if (err) reject(err);
+                        resolve(row)
+                    })
+                })
+
+
+        } catch (e) {
+            console.error(e)
+        }
+        return result;
+    },
+    runQuery: async function(query, args) {
+        if(!query) return console.error('No has ingresado una consulta')
+        try {
+            await sql.run(query, args)
+        } catch (e) {
+            console.error(e)
+        }
+    },
+}
--- src/masterdb.js
+++
@@ -1,3 +0,0 @@
-module.exports.runQuery = require('./queries.js/runQuery')
-module.exports.getQuery = require('./queries/getQuery')
-module.exports.allQuery = require('./queries/allQuery')
